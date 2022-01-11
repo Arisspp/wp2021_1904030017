c47b55ecffc041a852f6e0e85f51e542cbd5ad71
aris <arispangestu345@gmail.com>
2022-01-11 23:17:59 +0700

    menambahkan file pertemuan 3

---
+++ Pertemuan 3/Tugas1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Web Programming</title>
+</head>
+<body>
+    <h1>Tugas Web Programming 5B</h1>
+    <h2>Aris Pangestu</h2>
+    <h2>1904030012</h2>
+        <?php
+            $namadpn = "Aris";
+            $namablk = "Pangestu";
+            $namapgl = "Aris";
+            $umur = (0x14);
+            $ttl = "Serang, 25-11-2000";
+            $alamat = "Perum Permata Sepatan Blok C8/25";
+            $hobi = "Traveling";
+            $Mknfvrt = "Nasi Goreng";
+            $Mnfvrt = "Es Duren";
+            $Flmfvrt = "Avangers";
+        ?>
+    <hr>
+    <h2>Identitas Diri</h2>
+    <p>Nama : <?php echo $namadpn ." ". $namablk; ?></p>
+    <p>Nama Panggilan : <?php echo $namapgl; ?></p>
+    <p>Umur : <?php echo $umur; ?></p>
+    <p>Tempat Tanggal Lahir : <?php echo $ttl; ?></p>
+    <p>Alamat : <?php echo $alamat; ?></p>
+    <p>Hobi : <?php echo $hobi; ?></p>
+    <p>Makanan Favorit : <?php echo $Mknfvrt; ?></p>
+    <p>Minuman Favorit : <?php echo $Mnfvrt; ?></p>
+    <p>Film Favorit : <?php echo $Flmfvrt; ?></p>
+
+</body>
+</html>
